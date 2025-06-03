681b8913ba345938c4b17442f95466abfa43950b
Bar-Haim <Barhaim2147@gmail.com>
2025-06-03 16:40:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 pipeline {
-    agent none  // שולטת מההתחלה על איפה כל שלב ירוץ
+    agent any
 
     stages {
         stage('Echo') {
-            agent any  // השלב הזה ירוץ על כל agent פנוי
             steps {
                 echo 'hello world from the testtttt'
             }
